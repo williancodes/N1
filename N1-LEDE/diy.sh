c5e7749448c5c25a7ae349aaaf9563b5572051c8
jettlywillian <180314515+jettlywillian@users.noreply.github.com>
2026-03-29 03:40:10 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+# =========================================================
+# Description: N1-LEDE DIY Script
+# =========================================================
 
-# 删除lean库中的插件，使用自定义源中的包。
-rm -rf package/openclash
+# 1. 物理删除不需要的默认组件 (Passwall, SSR-Plus 等)
+# 在拉取完 feeds 后执行物理删除，防止被 make defconfig 自动勾选依赖
+echo ">> Removing unwanted packages..."
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/passwall
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-bypass
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 
+# 2. 修改默认 IP 为 10.0.0.2 (防御性编程：确保基础环境也被修改)
+echo ">> Setting default IP to 10.0.0.2..."
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
-# Add packages
-#添加科学上网源
-git clone --depth=1 --branch v0.46.011-beta https://github.com/vernesong/OpenClash package/openclash
+# 3. 安装指定版本的 OpenClash (修复目录结构识别错误)
+echo ">> Installing OpenClash (v0.46.011-beta)..."
+rm -rf package/luci-app-openclash
+# 克隆到临时目录
+git clone --depth=1 --branch v0.46.011-beta https://github.com/vernesong/OpenClash.git /tmp/OpenClash
+# 仅将需要的 luci-app-openclash 文件夹移动到 package 目录
+cp -rf /tmp/OpenClash/luci-app-openclash package/luci-app-openclash
+rm -rf /tmp/OpenClash
+
+# 4. 安装其他必需包 (主题等)
+echo ">> Installing Themes and amlogic..."
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
-
-
-# Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-
-#修改默认时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+# 5. 优化默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm" 2>/dev/null)
